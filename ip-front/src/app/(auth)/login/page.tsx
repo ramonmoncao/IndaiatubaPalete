52ae9d558810ed169212b32802d72971ace41508
Ramon Monção <ramonmoncao@hotmail.com>
2025-03-16 22:07:04 -0300

    Aprimoramento de responsividade

--- ip-front/src/app/(auth)/login/page.tsx
+++ ip-front/src/app/(auth)/login/page.tsx
@@ -9,7 +9,7 @@ export default function Login(){
             <div id="login-div">
                 <div id="images">
                     <Link href={'/home'}><Image src="/ipicon.png" alt="Palete" width={100} height={100} style={{filter : 'brightness(1000%)'}} /></Link>
-                    <Image src="/palete.png" alt="Palete" width={600} height={600} /></div>
+                    <Image src="/palete.png" id="palete" alt="Palete" width={600} height={600} /></div>
                 <div id = "box">
                     <form id ="form">
                         <label>Usuário</label>
