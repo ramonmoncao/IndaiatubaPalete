6b05182f06efd1be014a5e6e566a2564470912b8
Ramon Monção <ramonmoncao@hotmail.com>
2025-03-25 09:42:03 -0300

    Inicialização da estrutura

---
+++ ip-back/indaiatubapalete/src/main/java/com/projetointegrador/indaiatubapalete/IndaiatubapaleteApplication.java
@@ -0,0 +1,13 @@
+package com.projetointegrador.indaiatubapalete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndaiatubapaleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndaiatubapaleteApplication.class, args);
+	}
+
+}
---
+++ ip-back/indaiatubapalete/src/test/java/com/projetointegrador/indaiatubapalete/IndaiatubapaleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetointegrador.indaiatubapalete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndaiatubapaleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
